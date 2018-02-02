270fd571398d72461bf0707bd6230bdea7c30827
Molly Wojcik <mollywojcik@gmail.com>
2018-02-02 16:09:48 -0700

    twitter connection working

--- liri.js
+++ liri.js
@@ -1,13 +1,32 @@
+var request = require("request");
+var twitter = require('twitter');
 var twitterKeys = require("./keys.js");
 
 var command = process.argv[2];
 
 if (command == 'my-tweets') {
-  console.log('my-tweets');
-} else if (command == 'spotify-this-song') {
+  var client = new twitter(twitterKeys);
+  var params = {
+    screen_name: 'ninjaginja17',
+    count: 20,
+  }
+
+  client.get('statuses/user_timeline', params, function(error, data, response) {
+    if(!error) {
+    console.log(data);
+    console.log(response);
+    } else {
+    console.log(error);
+    }
+  });
+
+}
+  else if (command == 'spotify-this-song') {
   console.log('spotify-this-song');
-} else if (command == 'movie-this') {
+}
+  else if (command == 'movie-this') {
   console.log('movie-this');
-} else if (command == 'do-what-it-says') {
+}
+  else if (command == 'do-what-it-says') {
   console.log('do-what-it-says');
 }
