9e709700caa6e8a3f54c00afbe23e921cc2a6180
Molly Wojcik <mollywojcik@gmail.com>
2018-02-05 09:26:40 -0700

    Twitter, Spotify, OMDB all working

--- liri.js
+++ liri.js
@@ -14,7 +14,6 @@ if (command == 'my-tweets') {
 
   client.get('statuses/user_timeline', params, function(err, data, response) {
     if(!err) {
-    // console.log(data);
       for (var i = 0; i < data.length; i++) {
         console.log(data[i].text + '(' + data[i].created_at + ')');
       }
@@ -28,10 +27,9 @@ if (command == 'my-tweets') {
       id: '5c9e4058b9344f12b1a898462e0f0bd5',
       secret: 'a2f3387a2d284228a6ec1ee35318d0da'
     });
-
-    if (process.argv[3] != "") {
-      var input = process.argv;
-      var songName = "";
+    var input = process.argv;
+    var songName = "";
+    if (process.argv.length > 3) {
       for (var i = 3; i < input.length; i++) {
         if (i > 3 && i < input.length) {
           songName = songName + '+' + (input[i]);
@@ -43,21 +41,80 @@ if (command == 'my-tweets') {
       spotify.search({ type: 'track', query: songName }, function(err, data) {
         if (!err) {
           // console.log(JSON.stringify(data, null, 4));
-          console.log(data.tracks.items[0].artists[0].name);
-        } return console.log('Error occurred: ' + err);
+          console.log("------------------------------------------")
+          console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+          console.log('Song name: ' + songName.replace('+', ' ').replace(/(^|\s)[a-z]/g,function(f){return f.toUpperCase();}));
+          console.log('Preview link: ' + data.tracks.items[0].external_urls.spotify);
+          console.log('Album: ' + data.tracks.items[0].album.name);
+          console.log("------------------------------------------")
+        } else {
+          console.log('Error occurred: ' + err);
+        }
       });
-    } spotify
-        .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
-        .then(function(data) {
-          console.log('"' + data.name + '"' + ' by ' + data.album.artists[0].name);
-        })
-        .catch(function(err) {
-          console.error('Error occurred: ' + err);
+    } else {
+          spotify
+          .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+          .then(function(data) {
+            console.log("------------------------------------------")
+            console.log('"' + data.name + '"' + ' by ' + data.album.artists[0].name);
+            console.log("------------------------------------------")
+          })
+          .catch(function(err) {
+            console.error('Error occurred: ' + err);
+          });
+        }
+  }
+    else if (command == 'movie-this') {
+      // console.log('movie-this');
+      var input = process.argv;
+      var movieName = "";
+      if (input.length > 3) {
+        for (var i = 3; i < input.length; i++) {
+          if (i > 3 && i < input.length) {
+            movieName = movieName + '+' + (input[i]);
+          } else {
+            movieName += input[i];
+          }
+        }
+        var movieQueryUrl = "http://www.omdbapi.com/?apikey=trilogy&type=movie&plot=short&t=" + movieName;
+        request(movieQueryUrl, function(error, response, body) {
+          if (!error && response.statusCode === 200) {
+            console.log("------------------------------------------")
+            console.log("Movie title: " + JSON.parse(body).Title);
+            console.log("Released: " + JSON.parse(body).Year);
+            console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Produced in: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("------------------------------------------")
+          }
         });
-}
-  else if (command == 'movie-this') {
-  console.log('movie-this');
-}
-  else if (command == 'do-what-it-says') {
-  console.log('do-what-it-says');
-}
+
+      } else {
+          var mrNobodyUrl = "http://www.omdbapi.com/?apikey=trilogy&type=movie&plot=short&t=mr+nobody";
+          request(mrNobodyUrl, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+              // console.log(JSON.parse(body));
+              console.log("------------------------------------------")
+              console.log("Movie title: " + JSON.parse(body).Title);
+              console.log("Released: " + JSON.parse(body).Year);
+              console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+              console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);
+              console.log("Produced in: " + JSON.parse(body).Country);
+              console.log("Language: " + JSON.parse(body).Language);
+              console.log("Plot: " + JSON.parse(body).Plot);
+              console.log("Actors: " + JSON.parse(body).Actors);
+              console.log("------------------------------------------")
+            }
+          });
+        }
+
+    }
+      else if (command == 'do-what-it-says') {
+      console.log('do-what-it-says');
+
+      } else {
+        console.log("Please enter a command.");
+        }
